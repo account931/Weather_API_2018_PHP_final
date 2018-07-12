a916e7cc84abf19fa90a0db8dda9c1a063bf2f5a
account931 <account931@ukr.net>
2018-07-12 15:00:15 +0300

    Add files via upload
    mis commit fix

--- index.php
+++ index.php
@@ -1,193 +1,248 @@
-<!DOCTYPE html>
-  <html>
+<!doctype html>
+<!--------------------------------Bootstrap  Main variant ------------------------------------------>
+  <html lang="en-US">
     <head>
-      <title>Store Locator</title>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta charset="utf-8">
+      <meta http-equiv="Content-Type" content="text/html">
+      <meta name="description" content="Weather in Ukraine" />
+      <meta name="keywords" content="weather, weather in Ukraine, weather in Kiev, weather in the world">
+      <title>Weather</title>
+  
+      <!--Favicon-->
+      <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+	  
 
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	 
+	  
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-      <script src="js/storeLocator_core.js"></script><!--  Core Sore locator JS-->  
-	  <script src="js/addMarkerFromModal.js"></script><!--  add a marker, runs interaction with modal windows--> 
-	  <script src="js/directionApi.js"></script><!--  Api part that draws route on map--> 
-      <link rel="stylesheet" type="text/css" media="all" href="css/myStoreLocator.css">
-	  
-	    <!--Favicon-->
-      <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
 
-  </head>
-  <body>
-  <center>
-     <div id="">
-   
-       <h2 class="myShadow"> 
-	     <img style="height:29px;" src="images/store-locator.png"/>
-	     Custom store locator
-	     <img style="height:29px;" src="images/store-locator.png"/><br>
-	  </h2>
-    </div>
-    <br>
- 
-  
-    <div class="wrapper grey">
-      <div class="container">
- 
-    <!-- Controls: show/hide markers, from/to dropdowns--> 
-    <div id="controls" class="row no-gutter"> <!-- CLASS no-gutter that fixes spaces between cols-->
+      <link rel="stylesheet" type="text/css" media="all" href="css/myWeathers.css">
+      <script src="core_js/weather_core.js"></script><!--  Core Random JS-->
+	  <script src="core_js/theme_switcher.js"></script><!--  Theme switcher JS-->
+	  
+	  
 	
-	    <!--Hide or Show markers-->
-	    <div class="col-md-2 col-xs-6  style="">
-            <input id="btn_Control" type="button" name="Button2" value="Hide markers" class="btn" />
-	    </div>
-	    
-		<!--Instruct-->
-		<div class="col-md-1 col-xs-6  style="">
-            <input id="btn_instruct" type="button" name="Button2" value="Instruct" class="btn" data-toggle='modal' data-target='#myModalInstructions' />
-	    </div>
-		 
-		<!-- <select><option>-->
-		<div class="col-md-3 col-xs-6 ">
-            <span id="destination" ></span>
-		</div>
-		
-		<!-- <select><option>-->
-		<div class="col-md-3 col-xs-6 dest2">	
-            <span id="destination2" ></span>
-		</div>
-		
-		<div class="col-md-2 col-xs-6">
-            <input id="btn_CalcRoute" type="button" name="Button" value="Calc the route" class="btn" >
-		</div>
-		
-		<div class="col-md-1 col-xs-6">
-		    <input id="btn_CalcRoute_Clear" type="button" name="Button" value="Clear" class="btn" >
-		</div>
-		
-    </div>
-	<br>
-
-
-    <!--Div holds Matrix distance info-->
-    <div id="distanceInfo"></div>
-
-
-    <!-- this div will hold your map -->
-    <div id="map" class="myShadow"></div>
-
-
-   <!-- this div will hold your store info -->
-   <div id="info_div" class="myShadow"></div>
-
-
-   <!--CONFIRM DELETE?? my pop up -->
-   <div id="myTil"></div>
-
-  </center>
-
-  </div> <!--END class="wrapper grey">-->
-  </div> <!--END class="container">-->
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  <!-- START Bootstrap Modal with fields to add a new marker(hidden by default) -->
-  <!--we add {data-toggle='modal' data-target='#myModal'} to button (in JS) which triggers  to open modal with Bootstrap, no additional JS is needed-->
-<div id="myModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Add a new marker</h4>
-      </div>
-      <div class="modal-body">
-        <p>Provide info for marker <span id="newMarkerCoords"></span></p>
-		
-		<label for="usr">Marker Name:</label>
-        <input type="text" class="form-control" id="formMarkerName">
-		<label for="usr">Marker Info:</label>
-        <input type="text" class="form-control" id="formMarkerInfo">
-		<label for="usr">Marker Description:</label>
-        <input type="text" class="form-control" id="formMarkerDescription">
-		
-  
-      </div>
-      <div class="modal-footer">
-	    <button type="button" class="btn btn-default" data-dismiss="modal" id="agreedAddToSQL">Add</button>
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-
-  </div>
-</div>
- <!-- END Modal with fields to add a new marker -->
- 
- 
- 
- 
- 
- 
- 
-    <!-- START Bootstrap Modal with Instructions(hidden by default) -->
-  <!--we add {data-toggle='modal' data-target='#myModal'} to button (in JS) which triggers  to open modal with Bootstrap, no additional JS is needed-->
-<div id="myModalInstructions" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title red">How to use API</h4>
-      </div>
-      <div class="modal-body ">
-	      <div class="radius-red"><!-- start block1-->
-              <p>Please know, you can use Google Maps API asserts with your markers stored in SQL DataBase at this page</p>
-	          <p>You can add your personal markers to SQL DB, delete them, draw the route between two markers and calculate ETA and distance</p>
 	  
-	          <p>1. To add your personal marker, please click any place at map and select an option to add a marker</p>
-	          <p>1. To delete your personal marker, please click the marker and select an option to delete a marker </p>
-	          <p>3. To calcuate ETA and distance between two personal markers and draw a route, please select start and stop destination and click "Calc the route" </p>
-	          <p>4. To hide a drawn route and distance info, please click "Clear" </p>
-	          <br></hr class="red">
-	      </div><!-- end block1-->
-	   
-	      <br>
-	      <h4 class="red">Your current markers</h4>
-	      <p id="listOfMarkers" class="radius-red"></p><!-- will hold js generated list-->
-  
-      </div>
-      <div class="modal-footer">
-	    <button type="button" class="btn btn-default" data-dismiss="modal" id="">OK</button>
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
+	  <meta name="viewport" content="width=device-width" />
 
-  </div>
-</div>
- <!-- START Bootstrap Modal with Instructions(hidden by default) -->
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
-  
-  <!-- CORE SCRIPT, runs the Google maps Load-->
-  <script src="https://maps.googleapis.com/maps/api/js?callback=initMap" async defer></script>
-  <!--<script src="https://maps.googleapis.com/maps/api/js?key=PASTE_YOUR_KEY_HERE&callback=initMap" async defer></script>-->
-</body>
+     </head>
+	 
+	 <?php
+	     //make sure that this script can be called from your application, u should define $SecurityConstant in main  index.php + logic in Classes/Security/checkSecurityConstant();
+	     define("SecurityConstant", "Security permitted");
+	 ?>
+
+     <body>
+	   
+       <div id="headX" class=" text-center myShadow colorAnimate" style ='background-color:lavender;padding:10px;'> <!--#2ba6cb;-->
+	   
+         <h1 id="h1Text">
+             <img id ="wLogo" class="shrink-large" src="images/weather2.png"/>		 
+		     <span id="textChange" class="textShadow"> Weather on-line 7 days</span> 
+		     <!--</span> <img src="http://78.media.tumblr.com/tumblr_m2hq5tt3ma1qav3uso1_400.gif" style="width:3%"/>--> 
+			 <img id ="wLogo2" src="images/weather.png"/>
+			 <p class="language"><a class="lang" href="#">UA</a></p>
+			 <p class="theme"><a class="them" href="#">*</a></p>
+			 
+		 </h1> 
+		   
+           <!--<p class="header_p">All cities weather processor</p>-->  <!--generates random lists, ramdomizes integers, etc--> <!--<span class="glyphicon glyphicon-duplicate"></span>-->  
+	   </div>
+	   
+	   <!--
+	   <div id="headX" class="jumbotron text-center gradient alert-info" style ='background-color:lavender;'> 
+           <h1 id="h1Text"> <span id="textChange"> Weather on-line</span> <span class="glyphicon glyphicon-tree-conifer"></h1>    
+           <p class="header_p">All cities weather processor  
+           </p>	   
+	   </div>
+	   -->
+
+
+
+         <br>
+         <div class="wrapper grey">
+    	   <div class="container">
+		   
+		   
+		   
+		      <div class="col-sm-4 col-xs-12 myShadow shrink" style="background-color:lavender;">
+			  
+			  
+             
+		           <!--------Form Start------>	
+                   <form action="" id="myFormZ" method="post">
+                      <div class="form-group">
+                          <label for="city" id="cityLable">Your city: <span class="glyphicon glyphicon-transfer"></span></label>
+                          <input type="text" class="form-control" id="citytext" name="qr" list="character">
+						  <datalist id="character">
+						      
+							  <option value="Amsterdam"></option>
+							  <option value="Anchorage"></option> 
+							  <option value="Anuradhapura"></option>
+							  <option value="Athens"></option>
+							  <option value="Copenhagen"></option>
+							  <option value="Delhi"></option>
+							  <option value="Dubai"></option>
+							  <option value="Honolulu"></option>
+							  
+							  <option value="Istanbul"></option>
+							  <option value="Kuala Lumpur"></option>
+							  <option value="Kuwait"></option>
+							  <option value="Kyiv"></option>
+							  
+							  <option value="London"></option>
+							  <option value="Manila"></option>
+							  <option value="Marrakech"></option>
+							  <option value="Miami"></option> 
+                              <option value="Milan"></option> 
+                              <option value="Moscow"></option> 							  
+                              <option value="Odessa,UA"></option>
+							  <option value="Rome"></option>
+							  <option value="Stockholm"></option>
+							  <option value="Sydney"></option>
+							  <option value="Tokyo"></option>
+							  <option value="Vienna"></option>
+							  <option value="Yakutsk"></option>
+							  <option value="Zhytomyr"></option> 
+							  
+							  
+							  
+							 
+							  
+							  
+							  
+						  </datalist>
+                     </div>
+					<!--
+                    <div class="form-group">
+                          <label for="pwd">Password:</label>
+                          <input type="password" class="form-control" id="pwd">
+                    </div>
+                    <div class="checkbox">
+                          <label><input type="checkbox"> Remember me</label>
+                    </div>
+					-->
+                   <button type="button" class="btn btn-default" id="submit">Submit</button>
+				   <button type="button" class="btn btn-default" id="clear">Clear</button>
+                   </form>				   
+                   <!---------END Form -------->				   
+				  
+				   
+				 </div> <!--END <div class="col-sm-4" style="background-color:lavender;">-->
+				
+				 
+				  <!--<div class="col-sm-1 col-xs-1" "></div>-->
+				 
+				 
+				 
+				 
+				 <!--------------------------- START Error window---------------------------->
+				<div class="row">
+				<div class="col-sm-6 col-xs-12" id="qrResult"> <!-- width:100% caused none-one line position in web view, normal in mobile--> 
+				</div> <!-- END <div class="col-sm-4 col-xs-12" id="qrResult">-->
+				</div>
+                <!------------------------- END Error window------------------------------>
+				 
+				 
+				 
+				 
+				 
+				 
+                <!--------------------------- START GENERATING Weather DIVS---------------------------->
+				<br>
+				<center>
+				
+				<hr class="lavender-color " style="width:98%;height:8px;">
+				
+				<!--- Header with city, population--->
+				<div class="col-sm-12 col-xs-12 lavender-color myShadow " id="weather_header">
+				    <!--Weather in <span id='cityName'>Kyiv</span> for 7 days--> <!-- Content NOT USED, content will be JS html-->
+				</div> 
+				<br><br>
+				
+				
+				<!----Weather Core result-->
+				<div class="col-sm-12 col-xs-12 myShadow" id="weatherResult"  > <!-- This div accept the JS html result--><!--style='word-wrap: break-word;'-->
+				       
+				
+				</div> <!-- END class="row weatherResult">-->
+				
+                <!------------------------- SEND  GENERATING Weather DIVS------------------------------>
+                
+
+				 
+				 
+				 
+				 
+				  <!---Bootstrap dropdown JSON-->
+				 <br><div  class="col-sm-12 col-xs-12" style="height:30px;"></div><!-- Just for margin-->
+				 
+    	         <div class="dropdown">
+                     <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">JSON
+                     <span class="caret"></span></button>
+                     <div class="dropdown-menu">
+					     <p id="pureJson"></p>
+                     
+                     </div>
+                     </div>
+    	         <!---Bootstrap dropdown JSON-->
+				 
+				 </center>
+				 <br><br><br><br><br>
+				 
+				 
+				 
+				 
+                                    
+     
+    			</div><!-- /.container -->	
+				  		
+    		</div><!-- /.wrapper -->
+
+                
+
+       
+		
+		
+		
+		  <!-----Footer ---->
+		        
+				<div class="footer "> <!--navbar-fixed-bottom  fixxes bootom problem-->
+				    <!--Contact: --> <strong>dimmm931@gmail.com</strong><br>
+					<?php  echo date("Y"); ?>
+				</div>
+		<!--END Footer ---->  
+		
+		
+		
+		
+		<!-- ##### -- Advertise -- #### -->
+		<!--
+		   <div class="ads ">
+		        <a target="_blank" href="https://www.facebook.com/events/165143454205556/" id="link">
+				    <img src="images/ads/sub_od.jpg" alt="Submerged"/>  
+				</a>	
+			</div>
+			-->
+		<!-- ##### -- END Advertise -- #### -->
+		
+		
+		
+		
+    
+    </body>
 </html>
+
+
+
+
+
+<?php
+
+// Record (with CLASS) all the  input  to  txt;  //;
+      include("Classes/RecordTxt.php");
+      RecordTxt::RecordAnyInput(array(/*$user*/), 'recordText/weather_log.txt');// Record  to  text;
+//End  Record;
+?>
